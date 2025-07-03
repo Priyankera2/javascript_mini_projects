7fc9770d5a06105822cbc37985e4dfc7b476a521
Priyanka <priyankera2@gmail.com>
2025-07-03 11:14:05 +0530

    Update random.js

--- random number generator/random.js
+++ random number generator/random.js
@@ -1,5 +1,6 @@
+const diceFaces = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685'];
 let roll=document.getElementById("roll");
 let p=document.getElementById("myh3");
 roll.onclick=function(){
-    p.textContent=Math.floor(Math.random()*6)+1;
+    p.textContent=diceFaces[Math.floor(Math.random()*6)+1];
 }
